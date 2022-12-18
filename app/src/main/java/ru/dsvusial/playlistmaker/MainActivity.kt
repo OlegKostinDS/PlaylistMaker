e0357223a12cc90895de97914c04361f75a1aa4b
OlegKostinDS <olegkostinds@gmail.com>
2022-12-18 22:41:14 +0300

    final homework

--- app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
@@ -1,11 +1,29 @@
 package ru.dsvusial.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+        val musicBtn = findViewById<Button>(R.id.music_btn)
+        val settingBtn = findViewById<Button>(R.id.setting_btn)
+        val searchBtnClickListener: View.OnClickListener =
+            object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+
+                }
+            }
+        searchBtn.setOnClickListener(searchBtnClickListener)
     }
+
 }
