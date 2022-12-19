350e809849214da17db59a91ee53da4f81c724ce
OlegKostinDS <olegkostinds@gmail.com>
2022-12-19 23:01:10 +0300

    final homework

--- app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
@@ -1,12 +1,12 @@
 package ru.dsvusial.playlistmaker
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +24,13 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         searchBtn.setOnClickListener(searchBtnClickListener)
+
+        musicBtn.setOnClickListener{
+
+        }
+        settingBtn.setOnClickListener{
+
+        }
     }
 
 }
