216cfff27c39a973310c4748ba9c7b8ff1129381
OlegKostinDS <olegkostinds@gmail.com>
2022-12-21 22:37:38 +0300

    corrected homework

--- app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/dsvusial/playlistmaker/MainActivity.kt
@@ -1,10 +1,9 @@
 package ru.dsvusial.playlistmaker
 
 
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 
@@ -17,16 +16,17 @@ class MainActivity : AppCompatActivity() {
         val searchBtn = findViewById<Button>(R.id.search_btn)
         val musicBtn = findViewById<Button>(R.id.music_btn)
         val settingBtn = findViewById<Button>(R.id.setting_btn)
-        val searchBtnClickListener: View.OnClickListener =
-            object : View.OnClickListener {
-                override fun onClick(p0: View?) {
-                }
-            }
-        searchBtn.setOnClickListener(searchBtnClickListener)
+
+        searchBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+        }
 
         musicBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MusicActivity::class.java))
         }
+
         settingBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
         }
     }
 
