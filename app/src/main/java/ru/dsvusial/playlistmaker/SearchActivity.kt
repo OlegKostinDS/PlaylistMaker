a51a4297b9ad88ef061a7fc7d799d581b286170b
OlegKostinDS <olegkostinds@gmail.com>
2023-01-07 18:30:49 +0300

    fourth homework task

--- app/src/main/java/ru/dsvusial/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/dsvusial/playlistmaker/SearchActivity.kt
@@ -1,14 +1,17 @@
 package ru.dsvusial.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    var tempEditTextString = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -23,6 +26,8 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 searchClearBtn.visibility = clearButtonVisibility(p0)
+                tempEditTextString = p0.toString()
+
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -32,6 +37,22 @@ class SearchActivity : AppCompatActivity() {
         }
         searchEditText.addTextChangedListener(textWatcherSearchBtn)
         searchClearBtn.setOnClickListener { searchEditText.text.clear() }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(PRODUCT_AMOUNT, tempEditTextString)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchEditText = findViewById<EditText>(R.id.search_search)
+        searchEditText.setText(savedInstanceState.getString(PRODUCT_AMOUNT))
+
+    }
+
+    companion object {
+        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
