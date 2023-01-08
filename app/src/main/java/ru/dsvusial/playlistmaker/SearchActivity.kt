b2adf20880b36101ac2e4d80d58d059e6a21ba3e
OlegKostinDS <olegkostinds@gmail.com>
2023-01-08 11:46:56 +0300

    final homework task

--- app/src/main/java/ru/dsvusial/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/dsvusial/playlistmaker/SearchActivity.kt
@@ -19,19 +19,17 @@ class SearchActivity : AppCompatActivity() {
         val searchClearBtn = findViewById<ImageView>(R.id.search_cancel_btn)
         searchClearBtn.visibility = View.GONE
 
+
         val textWatcherSearchBtn = object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 searchClearBtn.visibility = clearButtonVisibility(p0)
                 tempEditTextString = p0.toString()
-
             }
 
             override fun afterTextChanged(p0: Editable?) {
-
             }
 
         }
